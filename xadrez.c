61f22914ee10543d40b3167bf8f1109606f553e7
Hannah <baixinhamanes@hotmail.com>
2025-11-24 11:56:41 -0300

    desafio novato completo, movimentacao da rainha, bispo e torre, menus interativos

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,80 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int casas, opcao, peca, i = 0;
+    char direcao[50];
+
+    do {
+        printf("### Jogo de Xadrez ###\n\n");
+        printf("Selecione uma opcao\n");
+        printf("1. Iniciar\n");
+        printf("2. Regras\n");
+        printf("3. Sair\n");
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case 1:
+                do {
+                    printf("\nSelecione a peca que quer movimentar\n");
+                    printf("1. Rainha\n");
+                    printf("2. Bispo\n");
+                    printf("3. Torre\n");
+                    printf("4. Voltar\n");
+                    scanf("%d", &peca);
+
+                    switch (peca) {
+                        case 1:
+                            printf("Quantas casas voce quer andar com a Rainha? (Diagonais e laterais)\n");
+                            scanf("%d", &casas);
+                            printf("Para qual direcao?\n");
+                            scanf(" %49[^\n]", direcao);
+
+                            for (i = 0; i < casas; i++) {
+                                printf("\nRainha: %s\n", direcao);
+                            }
+                            break;
+                        case 2:
+                            printf("Quantas casas voce quer andar com o Bispo (Diagonais: Cima-Esquerda)?\n");
+                            scanf("%d", &casas);
+                            printf("Para qual direcao?\n");
+                            scanf(" %49[^\n]", direcao);
+                        
+                            while (i < casas) {
+                                printf("\nBispo: %s\n", direcao);
+                                i++;
+                            }
+                            break;
+                        case 3:
+                            printf("Quantas casas voce quer andar com a Torre? (Laterais: Cima, Esquerda)\n");
+                            scanf("%d", &casas);
+                            printf("Para qual direcao?\n");
+                            scanf(" %49[^\n]", direcao);
+                            i = 0;
+                            do {
+                                printf("\nTorre: %s\n", direcao);
+                                i++;
+                            } while (i < casas);
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            printf("Opcao invalida\n");
+                    }
+                } while (peca != 4);
+                break;
+            case 2:
+                printf("\nSelecione a peca desejada, o numero de casas e a direcao para onde voce quer que ela va.\n");
+                printf("Bispo: anda na diagonal. Ex: Cima Direita\n");
+                printf("Torre: anda para os lados e para cima e para baixo. Ex: Esquerda\n");
+                printf("Rainha: anda em qualquer direcao. Ex: Cima; Baixo Esquerda\n");
+                break;
+            case 3:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+                
+        }
+    } while (opcao != 3);
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
